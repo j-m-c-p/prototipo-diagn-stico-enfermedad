2cc3795a27f6421cc7518e1809b3510db7327fc6
j-m-c-p <jhonatancubi@gmail.com>
2017-03-07 09:07:34 -0500

    Create libreria.php
    en este archivo se encuentra el link a la carpeta de bootstrap

---
+++ libreria.php
@@ -0,0 +1,10 @@
+<!-- 
+* 
+* @versión: 1.0 
+* @modificado: 23 de febrero del 2017 
+* @autores: Jhonnatan cubides, Harley santoyo
+* 
+--> 
+
+<link rel="stylesheet"  href="bootstrap/css/bootstrap.css">
+
