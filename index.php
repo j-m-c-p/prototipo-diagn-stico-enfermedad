93d3e0be83518d99c8779d981b738382c2b28cf6
j-m-c-p <jhonatancubi@gmail.com>
2017-03-07 09:11:02 -0500

    Create index.php
    contiene el index de nuestro prototipo

---
+++ index.php
@@ -0,0 +1,62 @@
+<!-- 
+* 
+* @versión: 1.0 
+* @modificado: 23 de febrero del 2017 
+* @autores: Jhonnatan cubides, Harley santoyo
+* 
+--> 
+
+<?php
+include ('Operaciones.php');
+include ('libreria.php');
+
+$obj_o= new Lista;
+
+?>
+
+
+
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="js/funciones.js"></script>
+</head>
+<body>
+
+
+		<div class="page-header">
+		  		<h1><center>Signos <small>y sintomas</small></center></h1>
+			</div>
+
+			<div class='container' >
+		  					  <div class='row'></div>
+		  					  <br></br>
+		  
+							  <div class='row'>
+							    <div class='col-xs-12 col-md-2 '></div>
+							      <div class='col-xs-12 col-md-8 well'>
+							        
+							               
+							               	
+							               	
+							               		<?php include('angularjs.php'); ?>
+
+
+
+				
+							   	   </div>
+							   	<div class='col-xs-12 col-md-2 '></div>
+							  </div>
+			</div>
+
+			
+ 	
+
+	
+
+
+	
+
+</body>
+</html>
+
