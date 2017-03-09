71a097f99845067ad00c083e6ed38200d816312c
j-m-c-p <jhonatancubi@gmail.com>
2017-03-09 08:59:40 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,11 @@
 
 
 <?php 
-$servidor = "localhost";
+    $servidor = "localhost";
     $usuario = "root";
     $clave = "";
     $bd = "bd_diagnostico_clinico";
+
+    //Puede colocar s o n minúsculas para ver diferentes impresiones de prueba de SQL y otras características.
+	$sn_diagnostico_clinico = "s";
  ?>
